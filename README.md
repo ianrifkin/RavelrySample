a4262f2d32a629fa96e3488d94edfab106f8e19c
Ian Rifkin <ianrifkin@ianrifkin.com>
2012-07-18 13:37:16 -0400

    Change script to use sendmail to actually send the message as an HTML email

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Sample Ravelry Project
 by Ian Rifkin (ianrifkin@ianrifkin.com)
 v1.0 July 18, 2012
 
-Dependencies: LWP::Simple, Getopt::Long, JSON;
+Dependencies: LWP::Simple, Getopt::Long, JSON, /usr/sbin/sendmail;
 
 Command line perl script that will output a custom reminder 
 message about what projects you have to work on and to come
@@ -13,10 +13,17 @@ back to ravelry.com. It takes two arguments: the user and the user's API key.
 
 This could be scheduled as a cron job to email the results regularly.
 
-This is a small proof of concept to show ease of formatting/outputting the project JSON feed.
+The idea is to remind users about their love of crafting and ravelry with the
+implied goal of increasing return visits back to ravelry.com. Mostly this is 
+simply a small proof of concept to show ease of formatting/outputting the 
+project JSON feed.
+
+Tested using perl 5.8.8 on irifkin and vsmedile ravelry accounts.
 
 Future ideas:
   (1) Separate email content info separate text file for easier configuration
-  (2) oAuth support
-  (3) Web app to display info on an HTML page
-  (4) Web app to schedule the reminder emails
+      Consider using HTML::Template
+  (2) Flag for text or html email content
+  (3) oAuth support
+  (4) Web app to display info on an HTML page
+  (5) Web app to schedule the reminder emails
--- bin/rav.pl
+++ bin/rav.pl
@@ -4,16 +4,17 @@ use LWP::Simple;
 use Getopt::Long;
 use JSON;
 
-###########################
-# ianrifkin@ianrifkin.com #
-# v1.0, July 18, 2012     #
-###########################
+############################
+# Created by IanRifkin.com #
+# v1.0, July 18, 2012      #
+############################
 
 #Parse user and key options. Key from http://www.ravelry.com/help/api
 my $usage = "Usage: ./rav.pl --user=YOUR_RAVELRY_USERNAME --key=YOUR_RAVELRY_API_STRING";
-my ($user,$key);
+my ($user,$key,$email);
 unless(GetOptions('user=s' => \$user,
 		  'key=s'  => \$key,
+		  'email=s'  => \$email,
 		 )) {
     die("Could not parse options: $!\n");
 }
@@ -41,31 +42,34 @@ for (my $count = 0; $count < length(%$rav_data->{projects}); $count++) {
 }
 
 #Write the actual messages (content depends on how many unfinished projects the user has)
-my $msg = "Dear $rav_data->{user}->{name},\n\n";
+my $msg = "<p>Dear $rav_data->{user}->{name},</p>\n\n";
 if (scalar(keys %unfinished) == 0) {
-    $msg .= "Wow, you finished all of your crafting projects! Maybe you should take a few minutes to enter your upcoming projects on ravelry.com.";
+    $msg .= "<p>Wow, you finished all of your crafting projects! Maybe you should take a few minutes to enter your upcoming projects on ravelry.com.</p>";
 }
 elsif (scalar(keys %unfinished) == 1) {
-    $msg .= "It's time to get off the computer and get back to your crafting! You only have one project to work on, but get to it! Who knows what other projects you have in mind that you haven't taken the time to enter into ravelry.com! If you must be on the computer, you should spend that time on ravelry.com.\n";
+    $msg .= "<p>It's time to get off the computer and get back to your crafting! You only have one project to work on, but get to it! Who knows what other projects you have in mind that you haven't taken the time to enter into ravelry.com! If you must be on the computer, you should spend that time on ravelry.com.</p>\n";
 }
 else {
-    $msg .= "Whoa! It's seriously time to get off the computer and get back to your crafting! You have " . scalar(keys %unfinished) . " projects you could be working on! And who knows what other projects you have in mind that you haven't taken the time to enter into ravelry.com! If you must be on the computer, you should spend that time on ravelry.com.\nProjects you could be working on:\n";
+    $msg .= "<p>Whoa! It's seriously time to get off the computer and get back to your crafting! You have " . scalar(keys %unfinished) . " projects you could be working on! And who knows what other projects you have in mind that you haven't taken the time to enter into ravelry.com! If you must be on the computer, you should spend that time on ravelry.com.\nProjects you could be working on:</p>\n<ul>\n";
     while ( my ($url, $project_data) = each(%unfinished) ) {
-	$msg .= "\n" . '<a href="' . $url . '">' . %$project_data->{name} . '</a>';
+	$msg .= "\n<li>" . '<a href="' . $url . '">' . %$project_data->{name} . '</a></li>';
     }
-    $msg .= "\n";
+    $msg .= "</ul>\n";
 }
-$msg .= "\nBefore you go, be proud of what you have accomplished:\n";
-    while ( my ($url, $project_data) = each(%finished) ) {
-	$msg .= "\n" . '<a href="' . $url . '">' . %$project_data->{name} . '</a>';
-    }
-$msg .= "\nNothing? Don't worry, you'll have something soon. I'm sure of it!" if (scalar(keys %finished) == 0);
+$msg .= "\n<p>Before you go, be proud of what you have accomplished:</p><ul>\n";
+while ( my ($url, $project_data) = each(%finished) ) {
+    $msg .= "\n<li>" . '<a href="' . $url . '">' . %$project_data->{name} . '</a></li>';
+}
+$msg .= "\n<li>Nothing? Don't worry, you'll have something soon. I'm sure of it!</li>" if (scalar(keys %finished) == 0);
+$msg .= "</ul>\n";
+
+$msg .= "\n\n<br /><p>Sincerely,\n<br />Ravelry</p>\n";
+
+#Send the output of the message using sendmail
+sendMail($email,$msg);
 
 
-$msg .= "\n\nSincerely,\nRavelry\n";
 
-#Print the output of the message to standard out.
-print "$msg";
 
 
 sub getFeed {
@@ -73,4 +77,19 @@ sub getFeed {
     my $content = LWP::Simple::get($url);
        die('JSON Feed Unavailable. Are you sure you supplied the correct username?') unless $content;
     return $content;
+}
+
+sub sendMail {
+    my ($email,$msg) = @_;
+    my $from= 'DoNotReply@ravelry.com';
+    my $subject='Ravelry Project Update';
+ 
+    open(MAIL, "|/usr/sbin/sendmail -t") or die("Cannot send mail. Cannot access /usr/sbin/sendmail: $!");
+    print MAIL "To: $email\n";
+    print MAIL "From: $from\n";
+    print MAIL "Subject: $subject\n";
+    print MAIL "MIME-Version: 1.0\n";
+    print MAIL "Content-Type: text/html\n\n";
+    print MAIL $msg;
+    close(MAIL);
 }
